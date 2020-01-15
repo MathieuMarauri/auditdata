26df6d5fa725bb425d58a6fd2f438d644eaeaba3
Mathieu Marauri <mathieu.marauri@mfglabs.com>
2020-01-15 15:06:47 +0100

    add cumulative argument to freq table for graphics in description functions (to come)

--- R/freq_table.R
+++ R/freq_table.R
@@ -5,48 +5,28 @@
 #'
 #' @param x the table containing the data,
 #' @param max_length the maximum number of rows in the output.
+#' @param cum shoulb cumulative frequencies and percentages be given? Default to FALSE
 #'
 #' @return a frequency table with the top 'max_length' most frequent
 #'   modalities and the associated percentage.
 #' 
 #' @import data.table
 #'
-freq_table <- function(x, max_length = Inf) {
+freq_table <- function(x, max_length = Inf, cum = FALSE) {
   N <- NULL
-  result <- as.data.table(x)[
-    ,
-    .N,
-    by = x
-    ][
-      order(-N),
-      list(
+  result <- as.data.table(x)[, .N, by = x][
+      order(-N),list(
         value = x,
         freq = N,
-        percent = round(100 * N / sum(N),
-                        digits = 0
-        )
-      )
-      ]
-  if (nrow(result) > max_length) {
-    result <- result[1:max_length, ]
-  }
-  return(result)
-}
-
-
-freq_table <- function(x, length_out = Inf, cum = FALSE){
-  result <- as.data.table(x)[ , .N, by = x] %>% 
-    .[order(-N), .(value = x,
-                   freq = N,
-                   percent = round(100 * N / sum(N),
-                                   digits = 0))]
-  if(cum) {
+        percent = round(100 * N / sum(N), digits = 0)
+      )]
+  if (cum) {
     result <- result[, .(value, freq, percent, 
                          cum_freq = cumsum(freq), 
                          cum_percent = cumsum(percent))]
   }
-  if(nrow(result) > length_out){
-    result <- result[1:length_out, ]
+  if (nrow(result) > max_length) {
+    result <- result[1:max_length, ]
   }
   return(result)
 }
