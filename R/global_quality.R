e25099bce405a67adba107cd41880c19131d988c
Mathieu Marauri <mathieu.marauri@mfglabs.com>
2020-02-12 17:17:50 +0100

    replace global quality function used for excel report with the one for the html report

--- R/global_quality.R
+++ R/global_quality.R
@@ -14,22 +14,102 @@
 #' @import data.table
 #'
 #' @export
-global_quality <- function(data, numeric_cutoff = -1, na_type = NA) {
-  if (is.data.table(data)) data <- as.data.table(data)
-  n_cols <- ncol(data)
-  n_rows <- nrow(data)
-  n_unique <- nrow(unique(data))
+# global_quality <- function(data, numeric_cutoff = -1, na_type = NA) {
+#   if (is.data.table(data)) data <- as.data.table(data)
+#   n_cols <- ncol(data)
+#   n_rows <- nrow(data)
+#   n_unique <- nrow(unique(data))
+#   if (!is.null(na_type)) {
+#     for (j in seq_along(data)) {
+#       set(data, i = which(data[[j]] %in% na_type), j = j, value = NA)
+#     }
+#   }
+#   types <- sapply(X = data, FUN = function(x) which_type(x, numeric_cutoff))
+#   n_miss <- colSums(is.na(data))
+#   percent_miss <- 100 * n_miss / n_rows
+#   n_unique_values <- sapply(data, uniqueN)
+#   output_global <- data.table(
+#     names(n_miss), types, n_miss,
+#     as.numeric(format(percent_miss, digits = 0)),
+#     n_unique_values
+#   )
+#   colnames(output_global) <- c(
+#     "Variable", "Type", "Missing values",
+#     "Percentage of missing values", "Unique values"
+#   )
+#   result <- list(global = output_global, dim = c("nrow" = n_rows, "ncol" = n_cols, "unique" = n_unique))
+#   return(result)
+# }
+
+
+#'
+#' This function analyzes the data quality of a table. It gives the dimensions of the
+#' table along with the number of unique values and missing values/ It also outputs a
+#' table with the number of unique values, the type and the percentage of missing values
+#' by variable.
+#'
+#' @param data the table to analyzed
+#' @param numeric_cutoff an integer specifying the minimal number of unique values
+#'   necessary for a vector not to be considered a factor. Default to -1
+#' @param na_type a character vector of strings that will be interpreted as NA
+#'
+#' @param return a list with two elements, one with global information (dimensions, unique
+#'   values and missing values) and a table with information for each variable.
+#'
+#' @export
+#' @import data.table
+#'
+global_quality <- function(data, numeric_cutoff = -1, na_type = NULL) {
+  # Arguments check
+  if (!is.data.frame(data) & !is.data.table(data)) {
+    stop("'data' must either be a data.frame or a data.table.")
+  }
+  if (!is.data.table(data)) data <- as.data.table(data)
+  if (!(is.numeric(numeric_cutoff) & length(numeric_cutoff) == 1)) {
+    stop("'numeric_cutoff' must be numeric of length one.")
+  }
+  if (!is.null(na_type)) {
+    if (!is.character(na_type)) {
+      stop("'na_type' must be a character vector.")
+    }
+  }
+  
+  # set vaues in na_type to NA
   if (!is.null(na_type)) {
     for (j in seq_along(data)) {
       set(data, i = which(data[[j]] %in% na_type), j = j, value = NA)
     }
   }
-  types <- sapply(X = data, FUN = function(x) which_type(x, numeric_cutoff))
+  
+  # global info
+  n_cols <- ncol(data)
+  n_rows <- nrow(data)
+  n_unique <- nrow(unique(data))
+  n_missing <- sum(is.na(data))
+  
+  # columns types
+  categorical_var <- which(sapply(
+    X = colnames(data),
+    FUN = function(name) is_categorical(data[[name]], numeric_cutoff = numeric_cutoff)) == TRUE)
+  numeric_var <- which(sapply(
+    X = colnames(data),
+    FUN = function(name) is_numeric(data[[name]], numeric_cutoff = numeric_cutoff)) == TRUE)
+  date_var <- which(sapply(
+    X = colnames(data),
+    FUN = function(name) is_date(data[[name]])) == TRUE)
+  
+  # data quality output
+  types <- rep(x = "undefined", length = n_cols)
+  types[categorical_var] <- "character"
+  types[numeric_var] <- "numeric"
+  types[date_var] <- "date"
   n_miss <- colSums(is.na(data))
   percent_miss <- 100 * n_miss / n_rows
   n_unique_values <- sapply(data, uniqueN)
   output_global <- data.table(
-    names(n_miss), types, n_miss,
+    names(n_miss),
+    types,
+    n_miss,
     as.numeric(format(percent_miss, digits = 0)),
     n_unique_values
   )
@@ -37,9 +117,15 @@ global_quality <- function(data, numeric_cutoff = -1, na_type = NA) {
     "Variable", "Type", "Missing values",
     "Percentage of missing values", "Unique values"
   )
-  result <- list(global = output_global, dim = c("nrow" = n_rows, "ncol" = n_cols, "unique" = n_unique))
-  return(result)
+  return(list(
+    global = list(
+      n_cols = n_cols, n_rows = n_rows, n_unique = n_unique,
+      n_missing = n_missing
+    ),
+    table = output_global
+  ))
 }
+
 
 #'
 #' This function performs a quality analysis on numeric data. It outputs a table with the
